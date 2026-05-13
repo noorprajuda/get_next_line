a9a5465212693f38a17c7f5e0ff4a2caa247da1e
Marsetio Noorprajuda <mnoorpra@3-F-2.42heilbronn.de>
2026-05-13 10:38:09 +0200

    ADD .gitignore

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mnoorpra <mnoorpra@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 19:04:59 by mnoorpra          #+#    #+#             */
-/*   Updated: 2026/05/11 19:24:36 by mnoorpra         ###   ########.fr       */
+/*   Updated: 2026/05/13 10:32:37 by mnoorpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,8 @@ int	main(void)
 	int		line_count;
 
 	printf("=== Testing get_next_line ===\n\n");
-
-	/* Test 1: Read from loremipsum.txt */
 	printf("Test 1: Reading from loremipsum.txt\n");
+	//Test 1: Read from loremipsum.txt 
 	fd = open("./loremipsum.txt", O_RDONLY);
 	if (fd < 0)
 	{
@@ -39,7 +38,6 @@ int	main(void)
 	}
 	close(fd);
 	printf("Total lines read: %d\n\n", line_count);
-
 	/* Test 2: Read from simple.txt */
 	printf("Test 2: Reading from simple.txt\n");
 	fd = open("./simple.txt", O_RDONLY);
@@ -59,7 +57,6 @@ int	main(void)
 	}
 	close(fd);
 	printf("Total lines read: %d\n\n", line_count);
-
 	/* Test 3: Handle non-existent file */
 	printf("Test 3: Attempting to read non-existent file\n");
 	fd = open("./nonexistent.txt", O_RDONLY);
@@ -69,7 +66,6 @@ int	main(void)
 		return (0);
 	}
 	close(fd);
-
 	printf("\n=== All tests completed ===\n");
 	return (0);
 }
